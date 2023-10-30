43ce09423bb1ff67c857f9d64a0e109d0a984fb8
Imran <jomi095@mail.ru>
2023-10-30 16:42:13 +0300

    сделааал

---
+++ index.js
@@ -0,0 +1,2 @@
+import originalMod2 from "./welcome.js";
+console.log(`привет, юзер. Вижу ты зашел с ${originalMod2}`)
---
+++ welcome.js
@@ -0,0 +1,3 @@
+import os from 'node:os'
+let originalMod2 = os.type()
+export default originalMod2;
